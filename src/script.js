b42e4c2b7f975b91a0a5d9986ef9f7fd18812ee7
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-04-28 00:55:24 +0700

    feat: responsive header on mobile, tablet, desktop

--- src/script.js
+++ src/script.js
@@ -3,8 +3,8 @@ const darkButton = document.querySelector('.dark-button');
 darkButton.addEventListener('click', function () {
   document.documentElement.classList.toggle('dark');
 
-  document.documentElement.classList.contains('dark')
-    ? (darkButton.innerHTML = `
+  darkButton.innerHTML = document.documentElement.classList.contains('dark')
+    ? `
 <svg
   xmlns="http://www.w3.org/2000/svg"
   fill="none"
@@ -19,8 +19,8 @@ darkButton.addEventListener('click', function () {
     d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
   />
 </svg>
-`)
-    : (darkButton.innerHTML = `
+`
+    : `
 <svg
   xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 20 20"
@@ -33,7 +33,7 @@ darkButton.addEventListener('click', function () {
     clip-rule="evenodd"
   />
 </svg>
-      `);
+      `;
 });
 
 // offset menu
